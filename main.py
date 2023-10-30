47dfb503dd8545dabaea23f6d58ff8e58ad6c054
JSha91 <jamo.sharif@uzh.ch>
2023-10-30 12:12:03 +0100

    -corrected JSon files -implemented find_threats_for_assets function

--- main.py
+++ main.py
@@ -1,14 +1,49 @@
-# This is a sample Python script.
+import json
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def find_threats_for_asset(asset_input):
+    # Load the AssetTaxonomy.json file
+    with open('AssetTaxonomy.json', 'r', encoding='utf-8') as asset_file:
+        asset_taxonomy = json.load(asset_file)
 
+    # Load the ThreatTaxonomy.json file
+    with open('ThreatTaxonomy.json', 'r', encoding='utf-8') as threat_file:
+        threat_taxonomy = json.load(threat_file)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+    # Find the category for the given asset
+    category = None
+    for asset_entry in asset_taxonomy:
+        if asset_entry['Asset'].lower() == asset_input.lower():
+            category = asset_entry['Category']
+            break
 
+    if category:
+        # Find threats related to the category
+        threats = []
+        for threat_entry in threat_taxonomy:
+            if category in threat_entry['Affected assets']:
+                threats.append({
+                    "Threat Category": threat_entry["Threat Category"],
+                    "Threat": threat_entry["Threat"],
+                    "Description": threat_entry["Description"],
+                    "Potential Impact": threat_entry["Potential Impact"]
+                })
 
-# Press the green button in the gutter to run the script.
+        if threats:
+            return threats
+        else:
+            return "No threats found for the category: " + category
+    else:
+        return "No threats found for the entered asset. Please check the spelling"
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    asset_input = input("Enter an asset: ")
+    threats = find_threats_for_asset(asset_input)
+    if isinstance(threats, str):
+        print(threats)
+    else:
+        for threat in threats:
+            print("Threat Category:", threat["Threat Category"])
+            print("Threat:", threat["Threat"])
+            print("Description:", threat["Description"])
+            print("Potential Impact:", threat["Potential Impact"])
+            print()
